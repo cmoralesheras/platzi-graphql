20436efd6f3c8b1c371e63d2f74d1be80fe5353e
Carlos Morales <carlosm3415@gmail.com>
2020-05-03 18:39:18 -0600

    cambios

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict'
+
+require('dotenv-flow').config()
+const app = require('express')()
+const { makeExecutableSchema } = require('graphql-tools')
+const gqlMiddleware = require('express-graphql')
+const { readFileSync } = require('fs')
+const { join } = require('path')
+const PORT = process.env.PORT || 5000
+const resolvers = require('./lib/resolvers')
+
+const typeDefs = readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+app.use('/api', gqlMiddleware({ schema, rootValue: resolvers, graphiql: true }))
+app.listen(PORT, () => console.log('YA INICIE EL PUERTO'))
